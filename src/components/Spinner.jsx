9cf59b6491822bd5a5a460ce97c02ca2588897ee
Nilesh <padhannilesh5@gmail.com>
2024-07-31 22:34:36 +0530

    Shopping Cart Website
    It is a Basic Shopping Website created using React.

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,14 @@
+
+
+import React from 'react'
+import './Spinner.css'
+
+const Spinner = () => {
+  return (
+    <div>
+    <div className="lds-circle flex flex-col items-center justify-center relative top-72"><div></div></div>
+    </div>
+  )
+}
+
+export default Spinner
